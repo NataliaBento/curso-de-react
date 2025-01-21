914879332c8500bb0c4cacdce5f197a02a1a8a1f
Natalia Bento <nataliabento97@gmail.com>
2025-01-20 21:43:19 -0300

    feat: props, lucide, botões

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
+import { useState } from "react";
 import AddTask from "./componentes/AddTask";
 import Tasks from "./componentes/Tasks";
 
-function App(){
-
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar programação",
+      description:
+        "Estudar programação para se tornar um desenvolvedor fullstack.",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar Inglês",
+      description: "Estudar inglês para se tornar fluente.",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Estudar Matemática",
+      description:
+        "Estudar Matemática para se tornar um desenvolvedor fullstack.",
+      isCompleted: false,
+    },
+  ]);
   return (
-
-    <div>
-      <h1>Gerenciador de Tarefas</h1>
-      <AddTask />
-      <Tasks />
-
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask />
+        <Tasks tasks={tasks} />
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
