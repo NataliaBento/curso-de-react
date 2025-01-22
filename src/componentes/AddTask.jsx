accb84d28e1eb6524ea31e8b20e42e9c69981d2c
Natalia Bento <nataliabento97@gmail.com>
2025-01-21 21:23:26 -0300

    feat: criacao de componentes

--- src/componentes/AddTask.jsx
+++ src/componentes/AddTask.jsx
@@ -1,7 +1,41 @@
-function AddTask(){
+import { useState } from "react";
 
-    <h1>Add Tasks</h1>
-
+function AddTask({ onAddTaskSubmit }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  return (
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+      <input
+        type="text"
+        placeholder="Digite o título da tarefa"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={title}
+        //SEMPRE QUE VC USAR JAVASCRIPT DENTRO DO RETURN É COM CHAVES A FUNÇÃO
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Digite a descrição da tarefa"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
+      <button
+        onClick={() => {
+          //verificar se o título e a descrição estão preenchidos
+          if (!title.trim() || !description.trim()) {
+            return alert("Preencha o título e a descrição da tarefa.");
+          }
+          onAddTaskSubmit(title, description);
+          setTitle("");
+          setDescription("");
+        }}
+        className="bg-slate-500 text-white px-4 py-2 rounded-md"
+      >
+        Adicionar
+      </button>
+    </div>
+  );
 }
 
-export default AddTask
+export default AddTask;
